f4d04fb51ccb85fc41c5875d0330b867a0038785
Mmsimpson <Marshall1044@gmail.com>
2018-10-03 13:27:01 -0400

    web token is fully functional and edited queries and sql user table

--- backend/databasestructure/dbFoundation.sql
+++ backend/databasestructure/dbFoundation.sql
@@ -3,4 +3,6 @@ CREATE TABLE users (
     username character varying (200),
     password character varying (200),
     email character varying (200),
+    first character varying (200),
+    last character varying (200)
 );
