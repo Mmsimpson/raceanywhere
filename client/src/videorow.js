35870f31aeffa3c1500843066f247e4c98b33638
Mmsimpson <Marshall1044@gmail.com>
2018-10-12 13:46:17 -0400

    Added styling to accountscreen, videos and their information, and submit form.

--- client/src/videorow.js
+++ client/src/videorow.js
@@ -1,20 +1,19 @@
 import React from 'react';
+import './stylesheets/videorow.css';
 
 let VideoRow = (props) => 
     <li>
-        {/* // onClick= {() => { */}
-        {/* //     // or history.push (video.id "probably")
-        //     console.log("to video page")
-        // }} */}
-        <video
+        <video className="vid"
             controls
+            preload="auto"
             src={`${process.env.REACT_APP_FETCHREQ}/uploads/${props.video.video}`} >
-            
         </video>
-        <p> {props.video.river} </p>
-        <p> {props.video.riverlevel} </p>
-        <p> {props.video.racetime} </p>
-        <p> {props.video.classvalue} </p>   
+        <div className="info-container">
+            <p> {props.video.river} </p>
+            <p> {props.video.riverlevel} </p>
+            <p> {props.video.racetime} </p>
+            <p> {props.video.classvalue} </p>   
+        </div>
     </li>
 
 export default VideoRow;
