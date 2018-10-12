32535279d0c0091c2182fdabdeff2ad3c5c9b415
Mmsimpson <Marshall1044@gmail.com>
2018-10-12 09:37:35 -0400

    added 'why us' page and edited css on other pages

--- client/src/why-us.js
+++ client/src/why-us.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import NavBar from './navbar.js';
 import Appheader from './appheader.js';
+import './stylesheets/why-us.css';
 
-let WhyUs = () => 
-  
+let WhyUs = () =>   
     <div>
-        <Appheader />
-        <NavBar />
-        <section className="mission"> 
+        <div className="header">
+            <Appheader />
+            <NavBar />
+        </div>
+        <section> 
             <h1>Our Mission</h1>
-            <div className="mission-para">
+            <div className="paragraph">
                 <p>
                     Race Anywhere's mission is to allow for competitive whitewater activities anywhere. No more 
                     qualifying, schedule conflicts, class requirements, or entry fees! Get involved in the race scene 
@@ -17,9 +19,9 @@ let WhyUs = () =>
                 </p>
             </div>
         </section>
-        <section>
-            <h3>Submission Guidelines</h3>
-            <div className="guideline-para">
+        <section className="section guideline">
+            <h1>Submission Guidelines</h1>
+            <div className="paragraph">
                 <p>
                     You must submit raw POV footage that starts before the 'start line' and ends after the 'finish line'.
                     For example the Green River race begins in the eddy below Bridal and ends at the river left 
@@ -28,8 +30,7 @@ let WhyUs = () =>
                     videos will be accepted.
                 </p>
             </div>
-        </section>
+        </section>    
     </div>
-
 
 export default WhyUs;
