c77293f90a5f1113f51f5f395060cf0f1036fa6e
Mmsimpson <Marshall1044@gmail.com>
2018-10-17 02:34:46 -0400

    restructured the layout of the videos

--- client/src/racer_vid_row.js
+++ client/src/racer_vid_row.js
@@ -3,20 +3,20 @@ import './stylesheets/racervidrow.css';
 
 let RacerVideoRow = (props) => 
     <div className="video-main">    
-        <div className="vid-info">
-            <div className="name">
-                <p className="recent-username"> Submitted by {props.video.username} </p>
-                <video className="vid"
-                    controls
-                    preload="auto"
-                    src={`${process.env.REACT_APP_FETCHREQ}/uploads/${props.video.video}`} >
-                </video>
-            </div> 
-            <div className="info-container">
-                <p> {props.video.river} </p>
-                <p> {props.video.riverlevel} </p>
-                <p> {props.video.racetime} </p>
-                <p> {props.video.classvalue} </p>   
+        <div className="video-row">
+            <div className="video-column">
+                <div className="content">
+                    <p className="recent-username"> Submitted by {props.video.username}  </p>
+                    <video className="videos"
+                        controls
+                        preload="auto"
+                        src={`${process.env.REACT_APP_FETCHREQ}/uploads/${props.video.video}`} >
+                    </video>
+                    <p> {props.video.river} </p>
+                    <p> {props.video.riverlevel} </p>
+                    <p> {props.video.racetime} </p>
+                    <p> {props.video.classvalue} </p> 
+                </div>  
             </div>
         </div>
     </div>
--- client/src/videorow.js
+++ client/src/videorow.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './stylesheets/videorow.css';
 
-let VideoRow = (props) => 
+let VideoRow = (props) =>   
     <div className="video-main">
-        <div className="vid-info">
-            <video className="vid"
-                controls
-                preload="auto"
-                src={`${process.env.REACT_APP_FETCHREQ}/uploads/${props.video.video}`} >
-            </video>
-            <div className="info-container">
-                <p> {props.video.river} </p>
-                <p> {props.video.riverlevel} </p>
-                <p> {props.video.racetime} </p>
-                <p> {props.video.classvalue} </p>   
+        <div className="video-row">
+            <div className="video-column">
+                <div className="content">
+                    <video className="videos"
+                        controls
+                        preload="auto"
+                        src={`${process.env.REACT_APP_FETCHREQ}/uploads/${props.video.video}`} >
+                    </video>
+                    <p> {props.video.river} </p>
+                    <p> {props.video.riverlevel} </p>
+                    <p> {props.video.racetime} </p>
+                    <p> {props.video.classvalue} </p>   
+                </div>
             </div>
         </div>
     </div>
-
 export default VideoRow;
